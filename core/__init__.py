46ddc3e804827e8b51da598c32515a0539638049
Luke <luke@amphoraxe.ca>
2025-10-10 04:01:25 -0700

    cleanup: keep only amp_llm and amp_llm_v2

--- core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core application modules.
-
-This package contains the main application logic and menu system.
-"""
-from .app import AMPLLMApp, GracefulExit
-from .menu import MenuSystem, MenuItem
-
-__all__ = ['AMPLLMApp', 'GracefulExit', 'MenuSystem', 'MenuItem']
