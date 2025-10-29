4d941b7a39e102249b3c8aa1cef2dcdb5e1cde1e
emilyyzhang <emilyzhang54368@gmail.com>
2025-10-28 23:10:40 -0700

    please be sane

--- amp_llm_v3/standalone modules/nct_lookup/nct_api_registry.py
+++ amp_llm_v3/standalone modules/nct_lookup/nct_api_registry.py
@@ -120,7 +120,7 @@ class APIRegistry:
         APIDefinition(
             id="uniprot",
             name="UniProt",
-            description="Protein sequence and functional information database 2",
+            description="Protein sequence and functional information database 3",
             category="extended",
             client_class="UniProtClient",
             enabled_by_default=True
